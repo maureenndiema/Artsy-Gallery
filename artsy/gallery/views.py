1a10fcb09ca52cad7002d7f208550742d6c7dc17
maureenndiema <ndiemam@gmail.com>
2022-05-30 19:43:19 +0300

    creating views

--- artsy/gallery/views.py
+++ artsy/gallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+     images = Image.objects.all()
+     category = Category.objects.all()
+     locations = Location.get_location()
+
+     if 'location' in request.GET and request.GET['location']:
+        name = request.GET.get('location')
+        images = Image.view_location(name)
