62c81110ed877b9df9c8df6cbede6b6546358775
maureenndiema <ndiemam@gmail.com>
2022-05-30 19:00:52 +0300

    creating image test

--- artsy/gallery/tests.py
+++ artsy/gallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nairobi')
+        self.location.save_location()
+
+        self.category = Category(name='nature')
+        self.category.save_category()
+
+        self.image_pic = Image(id=1, title='image', description='This is photo gallery test',location=self.location,category=self.category)
