7e31047dabddf438250183aef224ca2f15a5fa68
maureenndiema <ndiemam@gmail.com>
2022-05-30 18:01:48 +0300

    class method for get location

--- artsy/gallery/models.py
+++ artsy/gallery/models.py
@@ -3,3 +3,25 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=60)
+
+    def __str__(self):
+       return self.name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def get_location(cls):
+        location = Location.objects.all()
+        return location    
+    
+    @classmethod
+    def upt_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    
+    def del_location(self):
+        self.delete()
+
+        
+        
+
