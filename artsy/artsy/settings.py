3cc5bc22ff62568dfd034f4ab960f2d9304da478
maureenndiema <ndiemam@gmail.com>
2022-05-30 17:52:12 +0300

    add database in settings.py

--- artsy/artsy/settings.py
+++ artsy/artsy/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'artsy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'artsy',
+        'USER': 'maureen',
+        'PASSWORD':'1234',
     }
 }
 
