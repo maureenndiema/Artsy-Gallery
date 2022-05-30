7273a7adc4084c20e68b29c6410bc04cbaf2492f
maureenndiema <ndiemam@gmail.com>
2022-05-30 22:29:46 +0300

    create footer.html

--- artsy/gallery/urls.py
+++ artsy/gallery/urls.py
@@ -2,3 +2,15 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+app_name = 'gallery'
+
+urlpatterns=[
+  url(r'^$', views.home, name='home'),
+  url(r'^search/', views.search_results, name='search'),
+] + static(
+  settings.MEDIA_URL, 
+  document_root = settings.MEDIA_ROOT
+)
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
