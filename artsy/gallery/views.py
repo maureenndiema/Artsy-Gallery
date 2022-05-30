508ea37e1294bec1152a1236cdf1c65ef174cfcf
maureenndiema <ndiemam@gmail.com>
2022-05-30 19:46:26 +0300

    create location image in views.py

--- artsy/gallery/views.py
+++ artsy/gallery/views.py
@@ -10,3 +10,23 @@ def home(request):
      if 'location' in request.GET and request.GET['location']:
         name = request.GET.get('location')
         images = Image.view_location(name)
+
+     elif 'category' in request.GET and request.GET['category']:
+        cat = request.GET.get('Category')
+        images = Image.view_category(cat)
+
+        return render(request, 'index.html', {"name":name,"images":images,"cat":cat })
+     return render(request,"index.html",{'images':images,'locations':locations,'category':category})
+
+
+def location_img(request, location):
+    images = Image.filter_by_location(location)
+    return render(request, 'location.html', {'location_img': images})
+
+def search_results(request):
+     if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'search.html', {"message": message, "images": searched_images})
