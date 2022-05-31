eb448090e2c019a72cff2fe136a8c1ec646c433e
maureenndiema <ndiemam@gmail.com>
2022-05-31 12:06:32 +0300

    create re-path

--- artsy/gallery/urls.py
+++ artsy/gallery/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url,re_path,admin
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,10 @@ app_name = 'gallery'
 urlpatterns=[
   url(r'^$', views.home, name='home'),
   url(r'^search/', views.search_results, name='search'),
+  re_path(r'^admin/$', admin.site.urls),
+    # re_path('', include('artsy.urls')),
+  re_path(r'^home/$', views.home, name='home'),
+
 ] + static(
   settings.MEDIA_URL, 
   document_root = settings.MEDIA_ROOT
