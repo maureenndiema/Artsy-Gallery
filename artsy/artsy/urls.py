92e84ec7fc6cf5ad969aef9c670b7c44f5813658
maureenndiema <ndiemam@gmail.com>
2022-05-31 20:16:01 +0300

    displaying images

--- artsy/artsy/urls.py
+++ artsy/artsy/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
-from . import views
+from django.urls import include,re_path
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    re_path(r'^admin/$', admin.site.urls),
-    re_path('', include('artsy.urls')),
-#     re_path(r'^home/$', views.home, name='home'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path('', include('gallery.urls')),
+
 ]
--- artsy/gallery/urls.py
+++ artsy/gallery/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url,re_path,admin
+from django.urls import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,15 +6,16 @@ from django.conf.urls.static import static
 app_name = 'gallery'
 
 urlpatterns=[
-  url(r'^$', views.home, name='home'),
-  url(r'^search/', views.search_results, name='search'),
-  re_path(r'^admin/$', admin.site.urls),
-    # re_path('', include('artsy.urls')),
+
   re_path(r'^home/$', views.home, name='home'),
 
-] + static(
-  settings.MEDIA_URL, 
-  document_root = settings.MEDIA_ROOT
-)
+  re_path(r'^search/', views.search_results, name='search'),
+  
+  
+] 
+# + static(
+#   settings.MEDIA_URL, 
+#   document_root = settings.MEDIA_ROOT
+# )
 if settings.DEBUG:
     urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
